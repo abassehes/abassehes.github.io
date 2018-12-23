64b5b489aa7bf0a929034c2d91e0317c60f667db
Andreas Bassehes <andreasbassehes@Andreass-MacBook-Pro.local>
2018-12-22 17:38:10 -0800

    added city search

--- antipode_site/myScript.js
+++ antipode_site/myScript.js
@@ -1,9 +1,12 @@
 function convertAntiPode(lat, long) {
-	// antiPodeLat: lat * opposite
-	// antiPodeLong: (180 - abs(long)) * opposite
-
 	let antiPodeLat = (lat * -1);
-	let antiPodeLong = (180 - Math.abs(long));
+	let antiPodeLong;
+
+	if (long > 0) {
+		antiPodeLong = (long - 180);
+	} else {
+		antiPodeLong = (long + 180);
+	}
 
 	mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzc2VoZXMiLCJhIjoiY2pwaHA0eDE4MHl2ZTN3bXBpeXJwb3lnZyJ9.VVk0qC0658u4H0W2ZzScbw';
 	
@@ -26,5 +29,20 @@ function showPosition(p) {
 	convertAntiPode(lat, long);
 }
 
+function inputCityState() {
+	L.mapbox.accessToken = 'pk.eyJ1IjoiYmFzc2VoZXMiLCJhIjoiY2pxMDZhNWR6MDdyNzN3bzY5Ym1hMHpzYiJ9.IxafqRoU96Jywsql6Kb3_Q';
+	let geocoder = L.mapbox.geocoder('mapbox.places')
+	let city = document.getElementById('cityState').value;
+	geocoder.query(city, showMap);
+	console.log(city);
+}
+
+function showMap(err, data) {
+	convertAntiPode(data.latlng[0], data.latlng[1]);
+}
+
 let btn = document.getElementById('mybutton');
 btn.addEventListener('click', findGPS);
+
+let cityState = document.getElementById('cityStateButton');
+cityState.addEventListener('click', inputCityState);
